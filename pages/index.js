43957cfe1b0e888cb4925ce32fdf3e1b183d7f5b
kingmrubisnis <kingmru.bisnis@gmail.com>
2026-01-05 00:37:53 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
-
-const JobBlog = dynamic(() => import('../components/JobBlog'), { ssr: true })
+import JobBlog from '../components/JobBlog'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>ASEAN Career Hub - Find Jobs in Southeast Asia | Latest Opportunities</title>
-        <link rel="canonical" href="https://your-domain.vercel.app" />
       </Head>
       <JobBlog />
     </>
