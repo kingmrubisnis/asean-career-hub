908b24fd1b831967faa39865539ba49eccb19db0
kingmrubisnis <kingmru.bisnis@gmail.com>
2026-01-05 00:31:28 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -24,8 +24,12 @@ export default function Document() {
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
+  
+        {/* Theme color */}
+        <meta name="theme-color" content="#e9523d" />
         
         {/* Google AdSense - Replace YOUR_PUBLISHER_ID with your actual ID */}
         <script 
